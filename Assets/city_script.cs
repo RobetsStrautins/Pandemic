6abf36cf59b1d7c64c975d470eda098958dca14c
RobetsStrautins <roberts.strautin@gmail.com>
2025-09-30 00:08:36 +0300

    git repo

---
+++ Assets/city_script.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class city : MonoBehaviour
+{
+    public int id;
+    public string cityName;
+    public float Xcord;
+    public float Ycord;
+    public List<city> connectedCities;
+
+}
