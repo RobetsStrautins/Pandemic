67e1062ec197928032e2acff09dbe4515460167c
RobetsStrautins <roberts.strautin@gmail.com>
2025-10-02 00:56:51 +0300

    updateing assets

--- Assets/city_script.cs
+++ Assets/city_script.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class city : MonoBehaviour
 {
     public int id;
-    public string cityName;
+    public string cityName = "London";
+    public TextMeshProUGUI cityLabel;
     public float Xcord;
     public float Ycord;
+    public string color;
     public List<city> connectedCities;
-
+    void Start()
+    {
+        cityLabel.text = cityName;
+    }
+    
 }
